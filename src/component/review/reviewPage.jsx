36e0d8d32c3c52570c26d3b592a041f306d6b372
easa <eisanodehi@gmail.com>
2020-10-21 23:07:26 +0330

    add review - html

--- src/component/review/reviewPage.jsx
+++ src/component/review/reviewPage.jsx
@@ -1,5 +1,55 @@
-import React from 'react'
-const ReviewPage = () => (
-  <div>This is review page</div>
-)
+import React, { useState, useContext } from 'react'
+import { Redirect } from 'react-router-dom'
+import { FirebaseContext } from '../firebase'
+import { statement } from '@babel/template';
+
+const INITIAL_MOVIE = {
+  title: 'movie title',
+  year: 'year of production',
+  review: 'a simple text written about this movie',
+  author: { name: 'easa', id: 'alksjfoii2joi4j' },
+  date: new Date(),
+  comments: [{
+    content: 'this is a good movie',
+    author: { name: 'easa', id: 'alksjfoii2joi4j' },
+    date: new Date()
+  }]
+}
+const ReviewPage = () => {
+  const [movie, setMovie] = useState(INITIAL_MOVIE)
+  const firebase = useContext(FirebaseContext)
+  const onAddComment = event => {
+    if (movie.addCommentURL)
+      return <Redirect to={movie.addCommentURL} />
+  }
+  const onLike = event => {
+    // TODO: direct firebase add like function
+  }
+  const { title, year, date, review, comments, author } = movie
+  return (
+    <div className="col-md-6 coffset-md-3">
+      <h2>{title} - ({year})</h2>
+      <hr />
+      <p>{review}</p>
+      <p><author>
+        by <a href={'/user/' + author.id}> {author.name} </a>
+        <small>at {date.toDateString()}</small>
+      </author></p>
+      <hr />
+      <div className="col-md-11 coffset-md-1">
+        <h6>Comments: </h6>
+        {comments.map(comment => (
+          <div className="comment"> {
+            <p> {comment.content}
+              <author> | by
+                <a href={'/user/' + comment.author.id}> {comment.author.name} </a>
+                <small>at {comment.date.toDateString()} </small>
+              </author>
+            </p>
+          } </div>
+        ))}
+      </div>
+    </div>
+  )
+}
 export default ReviewPage
--- src/component/route/contentSwitch.jsx
+++ src/component/route/contentSwitch.jsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Signup from '../signin/signup.jsx'
 import Signin from '../signin/signin'
 import Home from '../home/index.jsx'
+import ReviewPage from '../review/reviewPage.jsx'
 // TODO:  className="row"
 const ContentSwitch = () => (
   <div className="container">
@@ -10,6 +11,7 @@ const ContentSwitch = () => (
       <Route path="/signin" > <Signin /> </Route>
       <Route path="/signup" > <Signup /> </Route>
       <Route path="/home" > <Home /> </Route>
+      <Route path="/review" > <ReviewPage /> </Route>
     </Switch>
   </div>
 )
